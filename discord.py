f796c1f6f07c16a7dd128387713564106ad41487
Bach Tran <xuanbachtran02@gmail.com>
2025-02-24 11:56:43 -0800

    add course filtering to avoid unwanted course to break embed

--- discord.py
+++ discord.py
@@ -12,6 +12,14 @@ GRADESCOPE_BASE_URL = 'https://www.gradescope.com'
 
 load_dotenv()
 
+def parse_course_envstring(courses_str: str) -> set:
+    """
+    Parse course IDs from string in environement variables 
+    """
+    if not courses_str:
+        return set()
+    return set(courses_str.split(','))
+
 if __name__ == '__main__':
 
     embeds = []
@@ -22,11 +30,21 @@ if __name__ == '__main__':
     
     student_courses: t.List[Course] = gs.get_courses().student_courses
 
-    # get Spring 25 courses
-    sp25_courses = list(filter(
-        lambda course: course.term == 'Spring' and course.year == '2025',
+    term, year = os.getenv('COURSES_TERM'), os.getenv('COURSES_YEAR')
+    assert (term is not None) and (year is not None), "Must define term and year in env file"
+     
+    selected_courses = list(filter(
+        lambda course: course.term == term and course.year == year,
             student_courses.values()))
     
+    inc_courses = parse_course_envstring(os.getenv('COURSES_TO_INCLUDE'))
+    if inc_courses:
+        selected_courses = filter(lambda course: course.cid in inc_courses, selected_courses)
+    
+    exc_courses = parse_course_envstring(os.getenv('COURSES_TO_EXCLUDE'))
+    if exc_courses:
+        selected_courses = filter(lambda course: course.cid not in exc_courses, selected_courses)
+
     # non-past due assignments, submitted vs unsubmitted
     # past due assignments
 
@@ -35,7 +53,7 @@ if __name__ == '__main__':
     due_count, due_today_count = 0, 0
     nextdue_assign, nextdue_url, nextdue_crs, nextdue_dt = None, None, None, None
 
-    for course in sp25_courses:
+    for course in selected_courses:
         res = gs.get_assignments(course.cid)
         assignments = res.assignments
         assignments_with_dt = []
